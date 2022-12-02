26eef502b563ad17a1d01b8ae4b46952a3981467
kwokth4 <kwokth4@oocl.com>
2022-12-02 14:23:32 +0800

    refactor: changed wording and removed useless library

--- src/main/java/com/parkinglot/ParkingLot.java
+++ src/main/java/com/parkinglot/ParkingLot.java
@@ -1,30 +1,28 @@
 package com.parkinglot;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class ParkingLot {
 
-    public static int capacity=10;
-    private Map<Ticket, Car> parkedPostion = new HashMap<>();
+    public int capacity=10;
+    private final Map<Ticket, Car> parkedPosition = new HashMap<>();
     public ParkingLot(){}
     public ParkingLot(int capacity){
         this.capacity = capacity;
     }
     public Ticket park(Car car) {
-        if(parkedPostion.size() < capacity){
+        if(parkedPosition.size() < capacity){
             Ticket ticket = new Ticket();
-            parkedPostion.put(ticket, car);
+            parkedPosition.put(ticket, car);
             return ticket;
         }
         return null;
     }
 
     public Car fetch(Ticket ticket) {
-        Car car = parkedPostion.get(ticket);
-        parkedPostion.remove(ticket);
+        Car car = parkedPosition.get(ticket);
+        parkedPosition.remove(ticket);
         return car;
     }
 }
