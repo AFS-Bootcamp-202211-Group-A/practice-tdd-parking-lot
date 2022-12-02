e76bb07d4aee21953bad7a92723aea02ae003e49
MICHAEL NAM (DSENG-ISD-OOCLL/HKG) <NAMMI@OOCL.COM>
2022-12-02 20:13:06 +0800

    [story4] feat: should_return_NoAvailablePositionException_when_park_given_two_parking_slots_both_without_any_position_and_car

--- src/test/java/com/parkinglot/StandardParkingBoyTest.java
+++ src/test/java/com/parkinglot/StandardParkingBoyTest.java
@@ -211,4 +211,28 @@ public class StandardParkingBoyTest {
         );
         assertEquals("Unrecognized parking ticket.", exception.getMessage());
     }
+
+    @Test
+    void should_return_NoAvailablePositionException_when_park_given_two_parking_slots_both_without_any_position_and_car() {
+        //given
+        ParkingLot parkingLot1 = new ParkingLot(1);
+        ParkingLot parkingLot2 = new ParkingLot(1);
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        parkingLots.add(parkingLot1);
+        parkingLots.add(parkingLot2);
+        StandardParkingBoy standardParkingBoy = new StandardParkingBoy(parkingLots);
+        Car car1 = new Car();
+        Car car2 = new Car();
+        parkingLot1.park(car1);
+        parkingLot2.park(car2);
+        Car extraCar = new Car();
+
+        //when
+
+        //then
+        Exception exception = assertThrows(NoAvailablePositionException.class,
+                () -> standardParkingBoy.park(extraCar)
+        );
+        assertEquals("No available position.", exception.getMessage());
+    }
 }
