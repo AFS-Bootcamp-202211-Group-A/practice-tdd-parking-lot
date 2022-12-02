51c17312a65316b4cd9e766760c6bd1119c57188
Marie Chow <chowma3@oocl.com>
2022-12-02 13:43:24 +0800

    feat: implement should_return_car_when_fetch_given_a_parked_car_and_a_ticket

--- src/main/java/com/parkinglot/ParkingLot.java
+++ src/main/java/com/parkinglot/ParkingLot.java
@@ -3,7 +3,14 @@ package com.parkinglot;
 import java.util.HashMap;
 
 public class ParkingLot {
+    private HashMap<Ticket, Car> parkedPosition = new HashMap<>();
     public Ticket park(Car car) {
-        return new Ticket();
+        Ticket ticket = new Ticket();
+        parkedPosition.put(ticket, car);
+        return ticket;
+    }
+
+    public Car fetch(Ticket ticket) {
+        return parkedPosition.get(ticket);
     }
 }
