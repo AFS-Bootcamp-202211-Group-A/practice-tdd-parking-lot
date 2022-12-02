5ca991c7ccb0c13e25734100e55bbfb630436b1b
kwokth4 <kwokth4@oocl.com>
2022-12-02 13:40:27 +0800

    test: should_return_correct_car_when_fetch_given_two_ticket_and_two_parked_car

--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -27,4 +27,17 @@ public class ParkingLotTest {
         //should
         assertEquals(car, fetchedCar);
     }
+    @Test
+    void should_return_correct_car_when_fetch_given_two_ticket_and_two_parked_car(){
+        //given
+        ParkingLot parkingLot = new ParkingLot();
+        Car car1 = new Car();
+        Car car2 = new Car();
+        Ticket ticket1 = parkingLot.park(car1);
+        Ticket ticket2 = parkingLot.park(car2);
+        //when
+        Car fetchedCar1 = parkingLot.fetch(ticket1);
+        //should
+        assertEquals(car1,fetchedCar1);
+    }
 }
--- test_cases.md
+++ test_cases.md
@@ -9,21 +9,26 @@ When fetch car,
 Return a car
 
 Case 3 - 
+Given two parked car,
+When fetch car.
+Return correct car
+
+Case 4 - 
 Given a wrong ticket, 
 When fetch car, 
 Return null car
 
-Case 4 - 
+Case 5 - 
 Given a empty ticket, 
 When fetch car, 
 Return null car
 
-Case 5 - 
+Case 6 - 
 Given a used ticket, 
 When fetch car, 
 Return null car
 
-Case 6 - 
+Case 7 - 
 Given a full parking lot, and a car, 
 When part the car, 
 Return null ticket
