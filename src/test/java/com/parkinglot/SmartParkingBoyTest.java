b72cb976625c5e5c888998460e2ae74d03a9df8f
kwokth4 <kwokth4@oocl.com>
2022-12-02 20:58:59 +0800

    [Story5] test: should_return_second_parking_lot_when_park_given_first_of_the_two_parking_lot_is_full_and_car

--- src/test/java/com/parkinglot/SmartParkingBoyTest.java
+++ src/test/java/com/parkinglot/SmartParkingBoyTest.java
@@ -24,4 +24,20 @@ public class SmartParkingBoyTest {
         //should
         assertNotNull(ticket);
     }
+    @Test
+    void should_return_second_parking_lot_when_park_given_first_of_the_two_parking_lot_is_full_and_car(){
+        //given
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        ParkingLot parkingLot1 = new ParkingLot(1);
+        ParkingLot parkingLot2 = new ParkingLot();
+        parkingLots.add(parkingLot1);
+        parkingLots.add(parkingLot2);
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLots);
+        parkingLot1.park(new Car());
+        Car car = new Car();
+        //when
+        Ticket ticket = smartParkingBoy.park(car);
+        //should
+        assertEquals(parkingLot2,ticket.getParkingLotOfTheTicket());
+    }
 }
