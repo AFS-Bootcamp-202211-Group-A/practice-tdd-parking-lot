a84e1f1eea2925746cbaf8e12d31b38512f69bee
heinrich-oocl <117629815+heinrich-oocl@users.noreply.github.com>
2022-12-02 13:43:37 +0800

    feat: should_return_parked_car_when_car_parked_given_parking_lot_and_ticket

--- src/main/java/com/parkinglot/ParkingLot.java
+++ src/main/java/com/parkinglot/ParkingLot.java
@@ -1,11 +1,15 @@
 package com.parkinglot;
 
 public class ParkingLot {
+
+    private Car parkedCar;
+
     public Ticket park(Car car) {
+        this.parkedCar = car;
         return new Ticket();
     }
 
     public Car fetch(Ticket ticket) {
-        return null;
+        return this.parkedCar;
     }
 }
