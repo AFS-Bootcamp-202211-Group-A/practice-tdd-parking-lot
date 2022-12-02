060c2bc390d97f190dd417c97e220e78f87b2774
heinrich-oocl <117629815+heinrich-oocl@users.noreply.github.com>
2022-12-02 16:17:40 +0800

    feat: should_return_ticket_when_park_car_given_parking_lot_and_car

---
+++ src/main/java/com/parkinglot/StandardParkingBoy.java
@@ -0,0 +1,15 @@
+package com.parkinglot;
+
+public class StandardParkingBoy {
+
+    private ParkingLot parkingLot;
+
+    public StandardParkingBoy(ParkingLot parkingLot){
+        this.parkingLot = parkingLot;
+    }
+
+    public Ticket park(Car car){
+        return parkingLot.park(car);
+    }
+
+}
