fef9ded63d4d012124b0aeaa7c12fd71c9d062b9
MICHAEL NAM (DSENG-ISD-OOCLL/HKG) <NAMMI@OOCL.COM>
2022-12-02 16:24:32 +0800

    [story3] feat: should_return_parked_car_when_fetch_car_given_parking_lot_with_parked_car_and_parking_ticket

--- src/test/java/com/parkinglot/StandardParkingBoyTest.java
+++ src/test/java/com/parkinglot/StandardParkingBoyTest.java
@@ -2,6 +2,7 @@ package com.parkinglot;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class StandardParkingBoyTest {
@@ -18,5 +19,20 @@ public class StandardParkingBoyTest {
 
         //then
         assertNotNull(ticket);
+    }
+
+    @Test
+    void should_return_parked_car_when_fetch_car_given_parking_lot_with_parked_car_and_parking_ticket() {
+        //given
+        ParkingLot parkingLot = new ParkingLot();
+        Car car = new Car();
+        StandardParkingBoy standardParkingBoy = new StandardParkingBoy(parkingLot);
+        Ticket ticket = standardParkingBoy.park(car);
+
+        //when
+        Car fetchedCar = standardParkingBoy.fetch(ticket);
+
+        //then
+        assertEquals(car, fetchedCar);
     }
 }
