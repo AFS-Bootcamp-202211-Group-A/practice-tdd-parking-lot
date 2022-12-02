54ecbb825550492151642bb9119af7c924a75019
Marie Chow <chowma3@oocl.com>
2022-12-02 13:44:37 +0800

    feat: implement should_return_right_car_when_fetch_twice_given_two_parked_car_and_two_ticket

--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -15,4 +15,30 @@ public class ParkingLotTest {
         // then
         assertNotNull(ticket);
     }
+    @Test
+    void should_return_car_when_fetch_given_a_parked_car_and_a_ticket() {
+        // given
+        ParkingLot parkingLot = new ParkingLot();
+        Car car = new Car();
+        Ticket ticket = parkingLot.park(car);
+        // when
+        Car fetchedCar = parkingLot.fetch(ticket);
+        // then
+        assertEquals(car, fetchedCar);
+    }
+    @Test
+    void should_return_right_car_when_fetch_twice_given_two_parked_car_and_two_ticket() {
+        // given
+        ParkingLot parkingLot = new ParkingLot();
+        Car aliceCar = new Car();
+        Car bobCar = new Car();
+        Ticket aliceTicket = parkingLot.park(aliceCar);
+        Ticket bobTicket = parkingLot.park(bobCar);
+        // when
+        Car aliceFetchedCar = parkingLot.fetch(aliceTicket);
+        Car bobFetchedCar = parkingLot.fetch(bobTicket);
+        // then
+        assertEquals(aliceCar, aliceFetchedCar);
+        assertEquals(bobCar, bobFetchedCar);
+    }
 }
