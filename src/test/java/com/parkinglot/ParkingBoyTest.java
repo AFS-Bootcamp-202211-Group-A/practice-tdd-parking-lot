fdd958d7b9bbcd0e97f3ea221970d7315d75a1a5
kwokth4 <kwokth4@oocl.com>
2022-12-02 16:20:42 +0800

    [Story3] test: should_return_correct_car_when_fetch_given_two_ticket_and_two_parked_car

--- src/test/java/com/parkinglot/ParkingBoyTest.java
+++ src/test/java/com/parkinglot/ParkingBoyTest.java
@@ -28,4 +28,18 @@ public class ParkingBoyTest {
         //should
         assertEquals(car, fetchedCar);
     }
+    @Test
+    void should_return_correct_car_when_fetch_given_two_ticket_and_two_parked_car(){
+        //given
+        ParkingLot parkingLot = new ParkingLot();
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+        Car car1 = new Car();
+        Car car2 = new Car();
+        Ticket ticket1 = parkingBoy.park(car1);
+        Ticket ticket2 = parkingBoy.park(car2);
+        //when
+        Car fetchedCar1 = parkingBoy.fetch(ticket1);
+        //should
+        assertEquals(car1,fetchedCar1);
+    }
 }
