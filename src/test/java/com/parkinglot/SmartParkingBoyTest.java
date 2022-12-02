9595d8779487f09c976e3442cc803918e7f3b44e
MICHAEL NAM (DSENG-ISD-OOCLL/HKG) <NAMMI@OOCL.COM>
2022-12-02 21:10:45 +0800

    [story] feat: should_return_NoAvailablePositionException_when_park_given_two_parking_slots_both_without_any_position_and_car

--- src/test/java/com/parkinglot/SmartParkingBoyTest.java
+++ src/test/java/com/parkinglot/SmartParkingBoyTest.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class SmartParkingBoyTest {
     @Test
@@ -46,5 +47,29 @@ public class SmartParkingBoyTest {
 
         //then
         assertEquals(car2, parkingLot1.fetch(ticket));
+    }
+
+    @Test
+    void should_return_NoAvailablePositionException_when_park_given_two_parking_slots_both_without_any_position_and_car() {
+        //given
+        ParkingLot parkingLot1 = new ParkingLot(1);
+        ParkingLot parkingLot2 = new ParkingLot(1);
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        parkingLots.add(parkingLot1);
+        parkingLots.add(parkingLot2);
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLots);
+        Car car1 = new Car();
+        Car car2 = new Car();
+        parkingLot1.park(car1);
+        parkingLot2.park(car2);
+        Car extraCar = new Car();
+
+        //when
+
+        //then
+        Exception exception = assertThrows(NoAvailablePositionException.class,
+                () -> smartParkingBoy.park(extraCar)
+        );
+        assertEquals("No available position.", exception.getMessage());
     }
 }
