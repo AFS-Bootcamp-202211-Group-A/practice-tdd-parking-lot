86aa3a00d42c86e43a1742a0dcf1b39eb8a448ec
MICHAEL NAM (DSENG-ISD-OOCLL/HKG) <NAMMI@OOCL.COM>
2022-12-02 21:09:20 +0800

    [story5] feat: should_park_first_parking_lot_when_park_given_two_parking_slot_and_first_few_empty_position_and_car

--- src/test/java/com/parkinglot/SmartParkingBoyTest.java
+++ src/test/java/com/parkinglot/SmartParkingBoyTest.java
@@ -27,4 +27,24 @@ public class SmartParkingBoyTest {
         //then
         assertEquals(car2, parkingLot2.fetch(ticket));
     }
+
+    @Test
+    void should_park_first_parking_lot_when_park_given_two_parking_slot_and_first_few_empty_position_and_car() {
+        //given
+        ParkingLot parkingLot1 = new ParkingLot();
+        ParkingLot parkingLot2 = new ParkingLot();
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        parkingLots.add(parkingLot1);
+        parkingLots.add(parkingLot2);
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLots);
+        Car car1 = new Car();
+        parkingLot2.park(car1);
+        Car car2 = new Car();
+
+        //when
+        Ticket ticket = smartParkingBoy.park(car2);
+
+        //then
+        assertEquals(car2, parkingLot1.fetch(ticket));
+    }
 }
