30219b8c17eeda1e0313634268b773fd353b0513
heinrich-oocl <117629815+heinrich-oocl@users.noreply.github.com>
2022-12-02 16:19:01 +0800

    feat: should_return_parked_car_when_car_parked_given_parking_lot_and_ticket

--- src/test/java/com/parkinglot/StandardParkingBoyTest.java
+++ src/test/java/com/parkinglot/StandardParkingBoyTest.java
@@ -22,6 +22,22 @@ public class StandardParkingBoyTest {
 
     }
 
+    @Test
+    public void should_return_parked_car_when_car_parked_given_parking_lot_and_ticket() {
+        //given
+        ParkingLot parkingLot = new ParkingLot();
+        StandardParkingBoy parkingBoy = new StandardParkingBoy(parkingLot);
+        Car car = new Car();
+
+        //when
+        Ticket ticket = parkingBoy.park(car);
+        Car parkedCar = parkingBoy.fetch(ticket);
+
+        //then
+        assertEquals(car, parkedCar);
+
+    }
+
 
 
 }
